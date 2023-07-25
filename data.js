d2d2e76762c447323c5e88a5729f62ce00c677c2
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2023-07-26 02:00:56 +0530

    First Commit

---
+++ data.js
@@ -0,0 +1,79 @@
+let songs = [{
+        name: 'Aaj Phir',
+        path: 'audio/Aaj Phir.mp3',
+        movie: 'Hate Story 2',
+        cover: 'img/Aaj Phir.jpg'
+    },
+    {
+        name: 'Ae Dil Hai Mushkil',
+        path: 'audio/Ae Dil Hai Mushkil.mp3',
+        movie: 'Ae Dil Hai Mushkil',
+        cover: 'img/Ae Dil Hai Mushkil.jpg'
+    },
+    {
+        name: 'Gerua',
+        path: 'audio/Gerua.mp3',
+        movie: 'Dilwale',
+        cover: 'img/Gerua.jpg'
+    },
+    {
+        name: 'Hamari Adhuri Kahani',
+        path: 'audio/Hamari Adhuri Kahani.mp3',
+        movie: 'Hamari Adhuri Kahani',
+        cover: 'img/Hamari Adhuri Kahani.jpg'
+    },
+    {
+        name: 'Hamdard',
+        path: 'audio/Hamdard.mp3',
+        movie: 'Ek Villain',
+        cover: 'img/Hamdard.jpg'
+    },
+    {
+        name: 'Hawayein',
+        path: 'audio/Hawayein.mp3',
+        movie: 'Jab Harry Met Sejal',
+        cover: 'img/Hawayein.jpg'
+    },
+    {
+        name: 'Kabhi Jo Baadal Barse',
+        path: 'audio/Kabhi Jo Baadal Barse.mp3',
+        movie: 'Jackpot',
+        cover: 'img/Kabhi Jo Baadal Barse.jpg'
+    },
+    {
+        name: 'Khairiyat',
+        path: 'audio/Khairiyat.mp3',
+        movie: 'Chhichhore',
+        cover: 'img/Khairiyat.jpg'
+    },
+    {
+        name: 'Muskurane',
+        path: 'audio/Muskurane.mp3',
+        movie: 'Citylights',
+        cover: 'img/Muskurane.jpg'
+    },
+    {
+        name: 'Sawan Aaya Hai',
+        path: 'audio/Sawan Aaya Hai.mp3',
+        movie: 'Creature 3D',
+        cover: 'img/Sawan Aaya Hai.jpg'
+    },
+    {
+        name: 'Suno Na Sangemarmar',
+        path: 'audio/Suno Na Sangemarmar.mp3',
+        movie: 'Youngistaan',
+        cover: 'img/Suno Na Sangemarmar.jpg'
+    },
+    {
+        name: 'Thodi Jagah',
+        path: 'audio/Thodi Jagah.mp3',
+        movie: 'Marjaavaan',
+        cover: 'img/Thodi Jagah.jpg'
+    },
+    {
+        name: 'Tum Hi Ho',
+        path: 'audio/Tum Hi Ho.mp3',
+        movie: 'Aashiqui 2',
+        cover: 'img/Tum Hi Ho.jpg'
+    }
+]
